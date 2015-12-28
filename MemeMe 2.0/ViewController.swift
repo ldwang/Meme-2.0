6d89249445630494008452bcf62fd6fc9ab2b840
Long(Daniel) Wang <wanglong@gmail.com>
2015-12-28 01:57:07 -0700

    Initial Commit

---
+++ MemeMe 2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe 2.0
+//
+//  Created by Long Wang on 2015-12-28.
+//  Copyright © 2015 Long Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
